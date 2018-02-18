3c2ce7299c200b416fc8a398ce56b5816ce903c6
nr83 <nr83@nyu.edu>
2018-02-18 12:32:23 +0400

    first commit

--- mininet_example1.py
+++ mininet_example1.py
@@ -14,8 +14,8 @@ net = Mininet( topo=topo, host=CPULimitedHost, link=TCLink)
 switch = net.addSwitch( 's1' )
 
 # Each host gets 50%/n of system CPU
-h1 = net.addHost( 'h%s' % (h + 1), cpu=.5/n)
-h2 = net.addHost( 'h%s' % (h + 1), cpu=.5/n)
+h1 = net.addHost( 'h1', cpu=.5/n)
+h2 = net.addHost( 'h2', cpu=.5/n)
 
 # 10 Mbps, 5ms delay, 2% # loss, 1000 packet queue
 net.addLink(h1, switch, bw=10, delay='5ms', loss=2, max_queue_size=1000, use_htb=True)
@@ -27,5 +27,5 @@ print "Testing network connectivity"
 net.pingAll()
 CLI(net)
 print "Testing bandwidth between h1 and h4"
-h1, h4 = net.get( 'h1', 'h4')
+h1, h2 = net.get( 'h1', 'h2')
 net.stop()
