fad2ed449f31460440c9ca1cda909ffb8c3828b9
nr83 <nr83@nyu.edu>
2018-02-18 12:09:34 +0400

    first commit

--- mytopo.py
+++ mytopo.py
@@ -5,27 +5,27 @@
 
 
 #How to test this program
-#sudo mn --custom myfirsttopo.py --topo myfirsttopo --test pingall
+#sudo mn --custom mytopo.py --topo mytopo --test pingall
 
 from mininet.topo import Topo
-class MyFirstTopo( Topo ):
+class MyTopo( Topo ):
     "Simple topology example."
     def __init__( self ):
         "Create custom topo."
         # Initialize topology
         Topo.__init__( self )
-            #Add hosts and switches
-            h1 = self.addHost( 'h1' )
-            h2 = self.addHost( 'h2' )
-            h3 = self.addHost( 'h3' )
-            h4 = self.addHost( 'h4' )
-            leftSwitch = self.addSwitch( 's1' )
-            rightSwitch = self.addSwitch( 's2' )
-            # Add links
-            self.addLink( h1, leftSwitch )
-            self.addLink( h2, leftSwitch )
-            self.addLink( leftSwitch, rightSwitch )
-            self.addLink( rightSwitch, h3 )
-            self.addLink( rightSwitch, h4 )
+        #Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2' )
+        # Add links
+        self.addLink( h1, leftSwitch )
+        self.addLink( h2, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, h3 )
+        self.addLink( rightSwitch, h4 )
 
-topos = { 'myfirsttopo': ( lambda: MyFirstTopo() ) }
+topos = { 'mytopo': ( lambda: MyTopo() ) }
