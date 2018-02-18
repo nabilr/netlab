f2f7abd701e649fdf7a45d30d2a830f525bda884
nr83 <nr83@nyu.edu>
2018-02-18 12:03:40 +0400

    first commit

---
+++ mytopo.py
@@ -0,0 +1,31 @@
+#Nabil Rahiman
+#NYU, Abudhabi
+#email:nr83@nyu.edu
+#18/Feb/2018
+
+
+#How to test this program
+#sudo mn --custom myfirsttopo.py --topo myfirsttopo --test pingall
+
+from mininet.topo import Topo
+class MyFirstTopo( Topo ):
+    "Simple topology example."
+    def __init__( self ):
+        "Create custom topo."
+        # Initialize topology
+        Topo.__init__( self )
+            #Add hosts and switches
+            h1 = self.addHost( 'h1' )
+            h2 = self.addHost( 'h2' )
+            h3 = self.addHost( 'h3' )
+            h4 = self.addHost( 'h4' )
+            leftSwitch = self.addSwitch( 's1' )
+            rightSwitch = self.addSwitch( 's2' )
+            # Add links
+            self.addLink( h1, leftSwitch )
+            self.addLink( h2, leftSwitch )
+            self.addLink( leftSwitch, rightSwitch )
+            self.addLink( rightSwitch, h3 )
+            self.addLink( rightSwitch, h4 )
+
+topos = { 'myfirsttopo': ( lambda: MyFirstTopo() ) }
